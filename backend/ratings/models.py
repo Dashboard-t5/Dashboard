a8104647fdc4c8406582ba4465feac1bce739f80
Дарья Федорова <dnfedorova@gmail.com>
2024-10-03 19:16:32 +0300

    Add Rating-model.

--- backend/ratings/models.py
+++ backend/ratings/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+
 from backend.config import MAX_LENGTH, MIN_LENGTH
+from backend.employees.models import Employee
 
 
 class Domain(models.Model):
@@ -63,6 +65,42 @@ class Skill(models.Model):
         verbose_name = "Навык"
         verbose_name_plural = "Навыки"
         ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+class Rating(models.Model):
+    """Модель оценки навыков сотрудников."""
+
+    RATING_CHOICES = [i for i in range(1,6)]
+
+    employee = models.ForeignKey(
+        Employee,
+        on_delete=models.CASCADE,
+        verbose_name="Сотрудник",
+    )
+    skill = models.ForeignKey(
+        Skill,
+        on_delete=models.CASCADE,
+        verbose_name="Оцениваемый навык",
+    )
+    rating_date = models.DateField(
+        verbose_name="Дата оценки",
+    )
+    rating_value = models.IntegerField(
+        verbose_name="Оценка сотрудника",
+        choices=RATING_CHOICES,
+    )
+    suitability = models.CharField(
+        max_length=MIN_LENGTH,
+        verbose_name="Соответствие",
+    )
+
+    class Meta:
+        verbose_name = "Оценка навыков сотрудника"
+        verbose_name_plural = "Оценки навыков сотрудников"
+        default_related_name = "ratings"
+        ordering = ("employee__last_name", "employee__first_name", "-rating_date",)
 
     def __str__(self):
         return self.name
