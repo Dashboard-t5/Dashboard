2241ff904f118c012796977d86b201c63dc4f5ab
Дарья Федорова <dnfedorova@gmail.com>
2024-10-04 00:42:23 +0300

    Admin_panel. First version.

--- backend/employees/admin.py
+++ backend/employees/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Position, Employee, Team
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    """Админка для модели Position."""
+
+    list_display = ("name",)
+    search_fields = ("name",)
+    list_filter = ("name",)
+    ordering = ("name",)
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    """Админка для модели Employee."""
+
+    list_display = ("first_name", "last_name", "position", "team", "grade")
+    search_fields = ("grade__name",)
+    list_filter = ("team", "position", "grade")
+    ordering = ("first_name", "last_name")
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    """Админка для модели Team."""
+
+    list_display = ("name",)
+    search_fields = ("name",)
+    list_filter = ("name",)
+    ordering = ("name",)
--- backend/ratings/admin.py
+++ backend/ratings/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Domain, Competence, Skill, Rating
+
+
+@admin.register(Domain)
+class DomainAdmin(admin.ModelAdmin):
+    """Админка для модели Domain."""
+
+    list_display = ("name",)
+    search_fields = ("name",)
+    list_filter = ("name",)
+    ordering = ("name",)
+
+@admin.register(Competence)
+class CompetenceAdmin(admin.ModelAdmin):
+    """Админка для модели Competence."""
+
+    list_display = ("name", "domain")
+    search_fields = ("name",)
+    list_filter = ("name",)
+    ordering = ("name",)
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    """Админка для модели Rating."""
+
+    list_display = ("employee", "skill", "rating_date", "rating_value", "suitability")
+    search_fields = ("skill__name",)
+    list_filter = ("skill", "rating_value", "suitability")
+    ordering = ("employee", "rating_date")
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    """Админка для модели Skill."""
+
+    list_display = ("name", "competence")
+    search_fields = ("name",)
+    list_filter = ("name",)
+    ordering = ("name",)
