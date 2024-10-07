5950c8d8c5b6881371e8ac1f3a1574e31f428e3b
Владимир Ласовский <herrShneider@gmail.com>
2024-10-07 22:27:29 +0200

    EmployeeSkillsAvgRating

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,24 +1,7 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
-
-from .views import (CompetenceViewSet, DomainViewSet, EmployeeViewSet,
-                    PositionViewSet, RatingViewSet, SkillViewSet,
-                    SuitabilityPositionViewSet, TeamViewSet)
 
 app_name = "api"
 
-router = DefaultRouter()
-router.register("positions", PositionViewSet, basename="positions")
-router.register("teams", TeamViewSet, basename="teams")
-router.register("employees", EmployeeViewSet, basename="employees")
-router.register("domains", DomainViewSet, basename="domains")
-router.register("skills", SkillViewSet, basename="skills")
-router.register("competences", CompetenceViewSet, basename="competences")
-router.register("raitings", RatingViewSet, basename="raitings")
-router.register(
-    "dashboard/suitability_position",
-    SuitabilityPositionViewSet,
-    basename="suitability_position"
-)
-
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("v1/", include("api.v1.urls"))
+]
---
+++ backend/api/v1/urls.py
@@ -0,0 +1,36 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from api.v1.views import (CompetenceViewSet,
+                          DomainViewSet,
+                          EmployeeViewSet,
+                          EmployeeSkillsViewSet,
+                          PositionViewSet,
+                          RatingViewSet,
+                          SkillViewSet,
+                          SuitabilityPositionViewSet,
+                          TeamViewSet)
+
+router_v1 = DefaultRouter()
+router_v1.register("positions", PositionViewSet, basename="positions")
+router_v1.register("teams", TeamViewSet, basename="teams")
+router_v1.register("employees", EmployeeViewSet, basename="employees")
+router_v1.register("domains", DomainViewSet, basename="domains")
+router_v1.register("skills", SkillViewSet, basename="skills")
+router_v1.register("competences", CompetenceViewSet, basename="competences")
+router_v1.register("raitings", RatingViewSet, basename="raitings")
+router_v1.register(
+    "dashboard/suitability_position",
+    SuitabilityPositionViewSet,
+    basename="suitability_position"
+)
+router_v1.register(
+    r"dashboard/suitability_position/(?P<employee_id>\d+)/skills",
+    EmployeeSkillsViewSet,
+    basename="employee_skills",
+)
+
+
+urlpatterns = [
+    path("", include(router_v1.urls)),
+]
