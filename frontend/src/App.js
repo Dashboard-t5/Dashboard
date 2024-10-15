4ab18c53ab9490af8834892c4b2124a6b0166aaf
unknown <ea.avdeev@gmail.com>
2024-10-15 10:19:10 +0300

    refactor(src): add modules css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
 import {useState} from 'react'
-import './App.css'
-import './globals.css'
 import Layout from './components/Layout/Layout'
 import NotFound from './components/NotFound/NotFound'
 import { TeamContext } from './context/context';
@@ -12,17 +10,16 @@ function App() {
   const [isTeamTotal, setTeamTotal] = useState(0)
 
   return (
-    <TeamContext.Provider value={{ isEmployeeId, setEmployeeId, isTeamId, setTeamId, isTeamTotal, setTeamTotal }}>
-      <Routes>
-        <Route exact path='/' index={true}
-               element={<Layout/>}>
-          {/*<Route index element={<Users />} />*/}
-          {/*<Route path="settings" element={<Settings />} />*/}
-        </Route>
-
-        <Route path='*' element={<NotFound/>}/>
-      </Routes>
-    </TeamContext.Provider>
+      <>
+        <TeamContext.Provider value={{ isEmployeeId, setEmployeeId, isTeamId, setTeamId, isTeamTotal, setTeamTotal }}>
+          <Routes>
+            <Route exact path='/' index={true}
+                   element={<Layout/>}>
+            </Route>
+            <Route path='*' element={<NotFound/>}/>
+          </Routes>
+        </TeamContext.Provider>
+      </>
   );
 }
 
